17b6e56e85dceecd14c4a700274f41e6730a8be3
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-24 05:04:07 +0530

    added Api key

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -5,6 +5,7 @@ import classes from './AuthForm.module.css';
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
@@ -15,32 +16,42 @@ const AuthForm = () => {
   const submitHandler = (event) => {
   event.preventDefault();
   
+  const enteredEmail = emailInputRef.current.value;
+  const enteredPassword = passwordInputRef.current.value;
+
+  // optinal : Add validation 
+
+  if(isLogin) {
+
+  }else {
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[AIzaSyCYklRhUwiUyBOu5-AaUW0otHqOiaLYDBE]"
+    );
+  };
   
   };
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form >
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input
-            type='password'
-            id='password'
-            required
-          />
+          <label htmlFor="password" ref={passwordInputRef}>
+            Your Password
+          </label>
+          <input type="password" id="password" required />
         </div>
         <div className={classes.actions}>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            { isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
