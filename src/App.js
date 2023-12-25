0186819e84543a04861f3f06c4aac9f959cfa79f
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-25 07:02:02 +0530

    logout handler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Switch, Route } from 'react-router-dom';
-
+import { Switch, Route, Redirect  } from 'react-router-dom';
+import { useContext } from 'react';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
@@ -7,17 +7,27 @@ import HomePage from './pages/HomePage';
 import AuthContextProvider from "./Store/Auth-context";
 
 function App() {
+  const AuthCtx = useContext(AuthContext); 
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
+        {!AuthCtx.isloggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+
+        {AuthCtx.isloggedIn && (
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+        )}
+        <Route path='*'>
+          <Redirect to='/'>
+          </Redirect>
         </Route>
       </Switch>
     </Layout>
