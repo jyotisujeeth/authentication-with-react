4acc4aa8ed5aed116cb8c25ec2baa45a7297e038
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-25 06:15:48 +0530

    change password han

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,39 @@
+import { useRef, useContext } from 'react';
+import AuthContextProvider from './Store/Auth-context';
+
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+const newPasswordInputRef = useRef();
+const authCtx =useContext(AuthContext);
+
+const SubmitHandler =(event) => {
+  event.preventDefault();
+//validation 
+
+fetch(
+  
+  "https://identitytoolkit.googleapis.com/v1/accounts:update?key=[API_KEY] ");
+  method: "POST",
+  body: JSON.stringify({
+    idToken: authCtx.token,
+    password,
+    returnSecureToken
+  }),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}).
+Then(ref => {
+  // assumation : successs 
+})
+};
+  const enteredPassword = newPasswordInputRef.currentValue;
   return (
     <form className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength="7" ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
